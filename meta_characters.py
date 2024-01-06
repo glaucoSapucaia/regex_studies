b7ca9e3dca71abde92439dfcbfad4624f1190dda
glaucoSapucaia <glaucopco@gmail.com>
2024-01-06 13:44:08 -0300

    Groups and rear views

--- meta_characters.py
+++ meta_characters.py
@@ -15,6 +15,15 @@
 
 8. backslash => Escape character 
 9. ? => non-greedy behavior and Range(0, 1)
+
+10. () => Define a group
+    Is saved in memory and can be reused => \1, \2, \3, ... (rear view)
+    Count rear views by opening parentheses =? 1( 2()) 3()
+    To not save and not return the group use => ?:
+
+    ONLY in PYTHON
+    Name a group => (?P<name>)
+    Use a name group => (?P=name)
 '''
 
 import re
@@ -58,6 +67,8 @@ enter2 = """
 <p>Lorem1</p> <p>Lorrrem2</p> <p>Lo</p> <div></div>
 """
 
+enter3 = '123.456.789-12'
+
 # | => or example
 print(re.findall(r'Quem|Sabe|primeiro', enter))
 print('_' * 100)
@@ -100,3 +111,38 @@ print('_' * 100)
 
 # non-greedy behavior => ?
 print(re.findall(r'<[dipv]{1,3}>.*?<\/[dipv]{1,3}>', enter2))
+print('_' * 100)
+
+# () => Define a group
+#     Is saved in memory and can be reused => 1, 2, 3, ...
+print(re.findall(r'<([dipv]{1,3})>.*?<\/\1>', enter2))
+print(re.findall(r'(<([dipv]{1,3})>.*?<\/\2>)', enter2))
+tags = re.findall(r'(<([dipv]{1,3})>.*?<\/\2>)', enter2)
+print()
+for tag in tags:
+    a, b = tag
+    print(a)
+tags2 = re.findall(r'(<([dipv]{1,3})>(.*?)<\/\2>)', enter2)
+print()
+for tag in tags2:
+    a, b, c = tag
+    print(c)
+print()
+
+# not save and not return an group => ?:
+print(re.findall(r'<([dipv]{1,3})>(?:.*?)<\/\1>', enter2))
+print('_' * 100)
+
+# more examples
+print(re.findall(r'[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}', enter3))
+print(re.findall(r'((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})', enter3))
+print(re.findall(r'[0-9]{3}\.|[0-9]{3}-|[0-9]{2}', enter3))
+print('_' * 100)
+
+# group with name
+print(re.findall(
+    r'<(?P<my_group>[dipv]{1,3})>(?:.*?)<\/(?P=my_group)>', enter2))
+print('_' * 100)
+
+# sub with groups
+print(re.sub(r'(<(.+?)>)(.*?)(<\/\2>)', r'\1ABC\4', enter2))
